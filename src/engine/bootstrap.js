3d1f9f8ab82871911dbd57ac5cfa2ec5e4dcf7e7
surunzi <surunzi@foxmail.com>
2016-02-20 22:40:16 +0800

    Dev: File structure init

---
+++ src/engine/bootstrap.js
@@ -0,0 +1,4 @@
+(function (ll)
+{
+
+}(liriliri));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-    entry: ''
+    entry: './core/liriliri.es6',
+    output: {
+        path: __dirname + '/src/engine',
+        filename: 'liriliri.js',
+        library: ['liriliri']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.es6$/,
+                loader: 'babel?presets[]=es2015'
+            }
+        ]
+    }
 };
